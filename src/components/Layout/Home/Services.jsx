9ac0070830ecb75fe1b9529cc050a03b6525e268
raju720190r <raju720190r@gmail.com>
2023-05-19 16:56:45 +0600

    Our Services Section

--- src/components/Layout/Home/Services.jsx
+++ src/components/Layout/Home/Services.jsx
@@ -3,8 +3,37 @@ import img2 from '../../../assets/sr-2.png'
 import img3 from '../../../assets/sr-3.png'
 const Services = () => {
     return (
-        <div className='my-6'> 
-            
+        <div className='my-6'>
+            <h2 className='text-center font-bold text-3xl my-6'>Our Services</h2>
+            <div className="grid lg:grid-cols-3 gap-5">
+                <div className="card bg-gray-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={img} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Money Return</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                </div>
+                <div className="card bg-gray-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={img2} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Support 24/7</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                </div>
+                <div className="card bg-gray-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={img3} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Free Shipping</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
